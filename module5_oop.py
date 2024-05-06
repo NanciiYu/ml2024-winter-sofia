24d161ec8e9c6d122c32c12f6833131d3770f64b
NanciiYu <168401827+NanciiYu@users.noreply.github.com>
2024-05-05 23:11:13 -0700

    Create module5_oop.py

---
+++ module5_oop.py
@@ -0,0 +1,30 @@
+class DataProcessor:
+    def __init__(self):
+        self.data = []
+
+    def insert_data(self, value):
+        self.data.append(value)
+
+    def search_data(self, target):
+        if target in self.data:
+            return self.data.index(target) + 1
+        else:
+            return -1
+
+def main():
+    processor = DataProcessor()
+
+    N = int(input("Enter the value of N: "))
+
+    print("Enter the numbers:")
+    for _ in range(N):
+        num = int(input())
+        processor.insert_data(num)
+
+    X = int(input("Enter the value of X: "))
+
+    index = processor.search_data(X)
+    print(index)
+
+if __name__ == "__main__":
+    main()
